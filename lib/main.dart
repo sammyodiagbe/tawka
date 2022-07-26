4b813618cae828f05c57dc41e8f4fa30f026cfc0
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-07-25 23:09:16 -0500

    working on the parking screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 // ignore_for_file: prefer_const_constructors
 import "package:flutter/material.dart";
+import 'package:tawka/screens/parking_screen.dart';
 
 void main() {
   runApp(TawkaApp());
 }
 
 class TawkaApp extends StatelessWidget {
+  const TawkaApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: ParkingScreen(),
     );
   }
 }
---
+++ lib/screens/parking_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ParkingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff3953dd),
+      // body: ,
+    );
+  }
+}
