294267f0ec935769cb79f401127609afa2736a52
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-07-31 07:16:35 -0500

    finished with custom navigation implementation

--- lib/widgets/custom_bottom_navigation.dart
+++ lib/widgets/custom_bottom_navigation.dart
@@ -9,31 +9,36 @@ class CustomBottomNavigationBar extends StatelessWidget {
   CustomBottomNavigationBar({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(),
-      height: 60,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          IconButton(
-            icon: FaIcon(FontAwesomeIcons.message),
-            onPressed: () {
-              Provider.of<Utility>(context, listen: false).updateScreen(0);
-            },
-          ),
-          IconButton(
-            icon: FaIcon(FontAwesomeIcons.user),
-            onPressed: () {
-              Provider.of<Utility>(context, listen: false).updateScreen(1);
-            },
-          ),
-          IconButton(
-            icon: FaIcon(FontAwesomeIcons.gear),
-            onPressed: () {
-              Provider.of<Utility>(context, listen: false).updateScreen(2);
-            },
-          ),
-        ],
+    return Consumer<Utility>(
+      builder: (context, provider, child) => Container(
+        decoration: BoxDecoration(),
+        height: 60,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              icon: FaIcon(FontAwesomeIcons.message),
+              color: provider.screen == 0 ? Colors.red : Colors.grey,
+              onPressed: () {
+                provider.updateScreen(0);
+              },
+            ),
+            IconButton(
+              icon: FaIcon(FontAwesomeIcons.user),
+              color: provider.screen == 1 ? Colors.red : Colors.grey,
+              onPressed: () {
+                provider.updateScreen(1);
+              },
+            ),
+            IconButton(
+              icon: FaIcon(FontAwesomeIcons.gear),
+              color: provider.screen == 2 ? Colors.red : Colors.grey,
+              onPressed: () {
+                provider.updateScreen(2);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
