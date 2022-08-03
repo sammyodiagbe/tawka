9e47b684cc9025ff9487d8de4cac27a388e5827f
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-08-02 22:07:49 -0500

    working with firebase

--- lib/services.dart/auth_service.dart
+++ lib/services.dart/auth_service.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 
 class AuthService implements BaseAuth {
   final _firebaseInstance = FirebaseAuth.instance;
